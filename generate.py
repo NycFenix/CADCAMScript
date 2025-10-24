a29cf638127de68989e6a3fc643e98aad99a8d95
Nycolas <88675719+NycFenix@users.noreply.github.com>
2025-10-24 09:31:27 -0300

    Added FreeCAD necessary libraries

--- generate.py
+++ generate.py
@@ -4,13 +4,22 @@ import sys
 
 sys.path.append(FREECADPATH) # Adiciona o path do FreeCAD ao sys.path
 
-
 import FreeCAD
-import Part
+import Part 
 
-# Teste: cria uma forma geométrica simples
+
+# Conversor de .stl/.step/.SLDPRT para g-code
+
+
+
+#path_parte = r"C:\Users\nycsf\Documents\Peças Teste\PL90-40S - PLACA 90° 40X40 SEM RECORTE"
+parte = FreeCAD.open("C:\\Users\\nycsf\\Documents\\Peças Teste\\Ronaldo 40 40.FCStd") # Freecad.open só aceita arquivos .FCStd. Passar os arquivos da peça por algum conversor?
+
+
 
 my_box = Part.makeBox(10, 20, 30)
 
+
 print(f"The volume of the box is: {my_box.Volume}")
 print(f"The area of the box is: {my_box.Area}")
+
